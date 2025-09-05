bd9698c034ba04b40f4b18a4875a36901302d322
Tommy Yick <tomyick100@gmail.com>
2025-09-05 14:16:40 -0400

    Python Basic Drills

---
+++ 9.5 Drills.py
@@ -0,0 +1,9 @@
+name = "Tommy"
+age = 25
+gpa = 3.20
+is_student = True
+
+print(f"My name is {name}")
+print(f"I am {age} years old")
+print(f"I have a GPA of {gpa}")
+print(f"Am I a student? {is_student}")
